5d5851d8429eeb7e711137de7b62c476b9aff0f5
Vram97 <shivaramsrikanth@gmail.com>
2023-07-04 19:03:11 -0400

    Solved combination sum2 using recursion

---
+++ combination_sum_2.cpp
@@ -0,0 +1,41 @@
+ class Solution {
+public:
+    vector<vector<int>> ans;
+    int sz;
+
+    void combo(vector<int>cand, vector<int>temp, int sum, int index,int target){
+        if(sum==target){
+            ans.push_back(temp);
+            return;
+        }
+
+        else if(sum>target || index>=sz){
+            return;
+        }
+        
+        else{
+            for(int i=index;i<sz;i++){
+                if(i!=index && cand[i]==cand[i-1]){
+                    continue;
+                }
+                temp.push_back(cand[i]);
+                combo(cand,temp,sum+cand[i],i+1,target-);
+                temp.pop_back();
+            }
+        }
+
+
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        sz=candidates.size();
+
+        vector<int> temp;
+
+        combo(candidates,temp,0,0,target);
+
+        return ans;
+
+        
+    }
+};
